1d83f5345ab92b4e231135819be59d48d9e1f714
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-09-09 14:07:37 +0700

    Главная страница

--- app.py
+++ app.py
@@ -3,6 +3,30 @@ import datetime
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/index")
+def index():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            <h1>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</h1>
+        </header>
+        <main>
+            <menu>
+                <li><a href="/lab1">Первая лабораторная</a></li>
+            </menu>
+        </main>
+        <footer>
+            Тырышкин Глеб Алексеевич, ФБИ-31, 3 курс, 2025
+        </footer>
+    </body>
+</html>
+"""
+
 @app.route("/lab1/web")
 def web():
     return """<!doctype html>
