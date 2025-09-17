09dcb4b4c6a6823cfeddee154146612392f8f402
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-09-17 18:44:18 +0700

    Имя студента

--- app.py
+++ app.py
@@ -354,5 +354,6 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Глеб Тырышкин'
+    return render_template('example.html', name=name)
 
