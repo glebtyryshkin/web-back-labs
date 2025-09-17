fbca6eff45a81a3f14c8baab949f0ec5552076df
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-09-17 17:00:08 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, abort
 
 app = Flask(__name__)
 
@@ -327,3 +327,12 @@ def a():
 @app.route('/lab2/a')
 def a_mod():
     return 'not ok'
+
+flower_list = ('роза', 'тюльпан', 'незабудка', 'ромашка')
+
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if 0 < flower_id >= len(flower_list):
+        abort(404)
+    else:
+        return "цветок: " + flower_list[flower_id]
