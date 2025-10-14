87da2568f236076f3bd0c4f056652edc77d229cc
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-10-14 22:48:29 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -79,3 +79,35 @@ def pay():
 def success():
     price = request.args.get('price', 0)
     return render_template('lab3/success.html', price=price)
+
+@lab3.route('/lab3/settings')
+
+def settings():
+    text_color = request.args.get('color')
+    bg_color = request.args.get('bg_color')
+    font_size = request.args.get('font_size')
+    font_weight = request.args.get('font_weight')
+
+    if any([text_color, bg_color, font_size, font_weight]):
+        resp = make_response(redirect('/lab3/settings'))
+        if text_color:
+            resp.set_cookie('color', text_color)
+        if bg_color:
+            resp.set_cookie('bg_color', bg_color)
+        if font_size:
+            if font_size.isdigit():
+                resp.set_cookie('font_size', font_size + 'px')
+            else:
+                resp.set_cookie('font_size', font_size)
+        if font_weight:
+            resp.set_cookie('font_weight', font_weight)
+        return resp
+
+    color = request.cookies.get('color')
+    bg = request.cookies.get('bg_color')
+    size = request.cookies.get('font_size')
+    weight = request.cookies.get('font_weight')
+
+    return render_template('lab3/settings.html',
+                           color=color, bg_color=bg,
+                           font_size=size, font_weight=weight)
