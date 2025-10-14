2e328939ced322442cd0a3f144a980ff1187ce42
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-10-15 00:03:24 +0700

    Исправление куков

--- lab3.py
+++ lab3.py
@@ -12,9 +12,10 @@ lab3 = Blueprint('lab3', __name__)
 
 
 def lab():
-    name = request.cookies.get('name')
+    name = request.cookies.get('name', 'Незнакомец')
     name_color = request.cookies.get('name_color')
-    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+    age = request.cookies.get('age', 'неизвестно')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color, age=age)
 
 @lab3.route('/lab3/cookie')
 
