530a1dceaca01ffce6a1fbc816ddb57c23d69bf2
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-10-29 16:30:27 +0700

    Заготовка к 5-ой лабораторной

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from lab3 import lab3
 
 from lab4 import lab4
 
+from lab5 import lab5
+
 
 app = Flask(__name__)
 
@@ -19,6 +21,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 @app.route('/')
 @app.route('/index')
