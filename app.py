54a248f1df5bcc67dd2f1af6c0b84d7d4721d6c4
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-12-09 20:51:35 +0700

    фиксы

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from lab5 import lab5
 
 from lab6 import lab6
 
+from lab7 import lab7
 
 app = Flask(__name__)
 
@@ -28,6 +29,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
+app.register_blueprint(lab7)
 
 @app.route('/')
 @app.route('/index')
