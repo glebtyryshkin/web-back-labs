17242ae14c6c239802b9eb12f33c018c5bbc5962
glebtyryshkin <glebtyryshkin091@gmail.com>
2025-12-24 16:18:13 +0700

    Авторизация

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from flask import Flask, url_for, request, redirect, abort, render_template
 
 from flask_sqlalchemy import SQLAlchemy
 from db import db
+from db.models import users
+from flask_login import LoginManager
 
 from lab1 import lab1
 
@@ -26,6 +28,14 @@ from lab8 import lab8
 from rgz import rgz
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return users.query.get(int(user_id))
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE' ] = os.getenv('DB_TYPE', 'postgres' )
--- lab8.py
+++ lab8.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, session
 from db import db
 from db.models import users
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from flask_login import login_user, login_required, current_user
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
@@ -36,3 +36,27 @@ def register():
     db.session.commit()
     
     return redirect('/lab8/') 
+
+@lab8.route('/lab8/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+
+    user = users.query.filter_by(login=login_form).first()
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember=False)
+            return redirect('/lab8/')
+    
+    return render_template('lab8/login.html', 
+                           error = 'Неверный логин или пароль')
+
+@lab8.route('/lab8/articles/')
+@login_required 
+def article_list():
+    return "список статей"
+    
